a92b8b5e6e42e340fa21f84ab4703513bf638a0d
Alex Williams <awilliams@alexwilliams.ca>
2015-09-20 07:28:53 +0000

    Add wrapper.sh for tests

---
+++ test/admin/bin/wrapper.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# Wrapper for executing admin commands
+#
+# Copyright (c) 2013-2015 Alex Williams, Unscramble. See the LICENSE file (MIT).
+# https://unscramble.co.jp
+#
+# VERSION: 1.0.0
+#
+#
+# Borrowed from: https://github.com/unscramble/jidoteki-admin
+
+admin_dir="."
+
+case "${SSH_ORIGINAL_COMMAND}" in
+  "update")
+    sudo ${admin_dir}/bin/update_vm.sh
+    ;;
+  "version")
+    if [ -f "${admin_dir}/etc/version.txt" ]; then cat ${admin_dir}/etc/version.txt; fi
+    ;;
+  "changelog")
+    if [ -f "${admin_dir}/etc/changelog.txt" ]; then cat ${admin_dir}/etc/changelog.txt; fi
+    ;;
+  "license")
+    sudo ${admin_dir}/bin/update_license.sh
+    ;;
+  "settings static")
+    sudo ${admin_dir}/bin/update_settings.sh static
+    ;;
+  "settings dhcp")
+    sudo ${admin_dir}/bin/update_settings.sh dhcp
+    ;;
+  "token")
+    sudo ${admin_dir}/bin/update_token.sh
+    ;;
+  "logs")
+    if [ -f "${admin_dir}/etc/logs.txt" ]; then
+      sudo ${admin_dir}/bin/update_logs.sh "$(cat ${admin_dir}/etc/logs.txt)";
+    fi
+    ;;
+  *)
+    exit 1
+    ;;
+esac
